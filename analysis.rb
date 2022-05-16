d63045b0c4bd40fee430cffc1cd33e35da926bbc
H <76665736+MeWhenI@users.noreply.github.com>
2022-05-16 12:25:32 -0500

    Update analysis.rb

--- analysis.rb
+++ analysis.rb
@@ -2,13 +2,13 @@
 script = `dd`
 
 # Normalize line-break encoding
-script.gsub!(2.chr,10.chr)
+script.gsub!(2.chr, 10.chr)
 
 # Remove page headings
-script.gsub!(/^\d+\.\n|Franklin Latt - CAA/,'')
+script.gsub!(/^\d+\.\n|Franklin Latt - CAA/, '')
 
 # Account for the various names used for Deborah throughout the script
-script.gsub!(/^THE GIRL|^FEMALE VOICE|^THE WAITRESS|^WAITRESS/,'DEBORA')
+script.gsub!(/^THE GIRL|^FEMALE VOICE|^THE WAITRESS|^WAITRESS/, 'DEBORA')
 
 # Get all dialogue line headings
 line_headings = script.scan(/^([A-Z ]*[A-Z])(\s\(.*\))?$/).map { |matches| matches[0] }
@@ -17,7 +17,7 @@ line_headings = script.scan(/^([A-Z ]*[A-Z])(\s\(.*\))?$/).map { |matches| match
 characters = line_headings.uniq
 
 # Get line counts per character
-lines_per_character = characters.map { |character| [character, line_headings.count(character)]}.to_h
+lines_per_character = characters.map { |character| [character, line_headings.count(character)] }.to_h
 
 # Get all scene headings
 scenes = script.split(/(?=\n\d+ [^\na-z]+\d+\n)/)
